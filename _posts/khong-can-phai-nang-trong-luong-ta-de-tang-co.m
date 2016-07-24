8d7f728711d04183ac0a96bc5e176a07f59693ba
nguyenphivn <vietphi.dang@gmail.com>
2016-07-24 18:57:02 +0700

    Update khong-can-phai-nang-trong-luong-ta-de-tang-co.m

--- _posts/khong-can-phai-nang-trong-luong-ta-de-tang-co.m
+++ _posts/khong-can-phai-nang-trong-luong-ta-de-tang-co.m
@@ -10,6 +10,15 @@ Nhưng trong một nghiên cứu mới đây, kết quả cho thấy việc nân
  *[You Don’t Need to Lift Heavy Weights to Build Muscle](http://vitals.lifehacker.com/you-don-t-need-to-lift-heavy-weights-to-build-muscle-1783854193?utm_campaign=socialflow_lifehacker_facebook&utm_source=lifehacker_facebook&utm_medium=socialflow)
  *[Neither load nor systemic hormones determine resistance trained-mediated hypertrophy or strength gains in resistance trained young men](http://jap.physiology.org/content/jap/121/1/129.full.pdf) | Chi tiết báo cáo 
  
- Một nhà nghiên cứu người Canada chọn ra 49 người đàn ông đã có kinh nghiệm tập luyện trong hai năm. Có hai điểm chúng ta cần lưu ý trong nghiên cứu này: 1. Có nhiều đối tượng tham gia vào thực nghiệm hơn khi so với các cuộc nghiên cứu tương tự 2. Các đối tượng này không bị ảnh hưởng bởi hiệu ứng của người mới bắt đầu (theo như được biết thì những người mới bắt đầu luyện tập với tạ sẽ có tốc độ tăng cơ nhanh hơn so với những người đã tập một thời gian)
+ Một nhà nghiên cứu người Canada chọn ra 49 người đàn ông đã có kinh nghiệm tập luyện trong hai năm. Có hai điểm chúng ta cần lưu ý trong nghiên cứu này: 
+ 
+  1. Có nhiều đối tượng tham gia vào thực nghiệm hơn khi so với các cuộc nghiên cứu tương tự.
+  2. Các đối tượng này không bị ảnh hưởng bởi hiệu ứng của người mới bắt đầu (theo như được biết thì những người mới bắt đầu luyện tập với tạ sẽ có tốc độ tăng cơ nhanh hơn so với những người đã tập một thời gian).
+ 
+ Nhóm được chia ra thành hai nhóm riêng biệt, nhóm nhiều-lần-đẩy (20 - 25 lần đẩy tạ) và nhóm ít-lần-đẩy ( 8 -12 lần đẩy). Mỗi nhóm luyện tập 4 ngày một tuần trong vòng 12 tuần, với khối lượng bài tập tương tự nhau. Chế độ ăn cũng không có gì khác biệt. Để chắc chắn rằng các đối tượng luyện tập với một cường độ đúng (tập đến hết sức - to failure), nhà nghiên cứu đã tập luyện với từng đối tượng và chắc rằng hầu hết các đối tượng không còn chút sức lực nào khi kết thúc mỗi hiệp.
+ 
+ Sau khi kết thúc 12 tuần, các nhà nghiên cứu đã tiến hành đo lường cơ bắp đùi của các đối tượng và phát hiện ra rằng cả hai nhóm đã tăng được lượng cơ bắp và sức mạnh tương tự nhau. Phát hiện này thực sự đã đi ngược lại với những gì mà trước đây chúng ta thường biết *luyện tập với nhiều số lần đẩy* sẽ không hiệu quả trong việc gia tăng sức mạnh (strength). Sức mạnh là sản phẩm của của cả hai hệ thần kinh và cơ bắp được sử dụng để nâng một trọng lượng nhất định. Nghiên cứu chỉ ra rằng bạn hoàn toàn có thể gia tăng sức mạnh tối đa của mình bằng việc luyện tập nhiều-lần-đẩy, miễn là bạn cũng nâng tạ nặng định kỳ.
+ 
+ Và cuối cùng, một điều thú vị khác mà nghiên cứu này cũng cho thấy: những hormon tăng trưởng và testosterone được cho là những tác nhân quan trọng trong việc làm gia tăng cơ bắp, không làm ảnh hưởng đến kết quả của nghiên cứu.
  
  
